7cd417770c289cf225742c65382d5144802f55ce
Richard <richard@RicharddeMac-mini.local>
2019-06-06 08:01:38 +0800

    debug

--- src/main/java/richard/test/classloader/TestClass.java
+++ src/main/java/richard/test/classloader/TestClass.java
@@ -3,6 +3,6 @@ package richard.test.classloader;
 public class TestClass {
 
     public void sayHello() {
-        System.out.println("This is me!");
+        System.out.println("This is me from richard.test.classloader.2.0!");
     }
 }
