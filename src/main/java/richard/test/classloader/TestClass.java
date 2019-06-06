3a970edc30de4c85d8afb9c64cf3c7b2eddf166c
Richard Liu <liuyngchng@hotmail.com>
2019-06-06 14:04:45 +0800

    debug

--- src/main/java/richard/test/classloader/TestClass.java
+++ src/main/java/richard/test/classloader/TestClass.java
@@ -2,7 +2,9 @@ package richard.test.classloader;
 
 public class TestClass {
 
+    public static String id = "1";
+
     public void sayHello() {
-        System.out.println("This is me from richard.test.classloader2!");
+        System.out.println("This is me from richard.test.classloader " + TestClass.id);
     }
 }
